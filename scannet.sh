b62605a148ef7ac384f41f464dbeed9f3683308c
Lingzhe Zhao <zhaolingzhe@westlake.edu.cn>
2025-05-01 17:46:28 +0800

    add scannet

--- scannet.sh
+++ scannet.sh
@@ -1,55 +1,23 @@
-./bin/replica_rgbd \
+./bin/scannet_rgbd \
     ./ORB-SLAM3/Vocabulary/ORBvoc.txt \
-    ./cfg/ORB_SLAM3/RGB-D/Replica/office0.yaml \
-    ./cfg/gaussian_mapper/RGB-D/Replica/replica_rgbd.yaml \
-    ./data/Replica/office0 \
-    ./outputs/Replica/office0 \
+    ./cfg/ORB_SLAM3/RGB-D/Scannet/common.yaml \
+    ./cfg/gaussian_mapper/RGB-D/Scannet/common.yaml \
+    ./data/Scannet/scene0059_00 \
+    ./outputs/Scannet/scene0059_00 \
     no_viewer
 
-./bin/replica_rgbd \
+    ./bin/scannet_rgbd \
     ./ORB-SLAM3/Vocabulary/ORBvoc.txt \
-    ./cfg/ORB_SLAM3/RGB-D/Replica/office1.yaml \
-    ./cfg/gaussian_mapper/RGB-D/Replica/replica_rgbd.yaml \
-    ./data/Replica/office1 \
-    ./outputs/Replica/office1 \
+    ./cfg/ORB_SLAM3/RGB-D/Scannet/common.yaml \
+    ./cfg/gaussian_mapper/RGB-D/Scannet/common.yaml \
+    ./data/Scannet/scene0106_00 \
+    ./outputs/Scannet/scene0106_00 \
     no_viewer
 
-./bin/replica_rgbd \
+    ./bin/scannet_rgbd \
     ./ORB-SLAM3/Vocabulary/ORBvoc.txt \
-    ./cfg/ORB_SLAM3/RGB-D/Replica/office0.yaml \
-    ./cfg/gaussian_mapper/RGB-D/Replica/replica_rgbd.yaml \
-    ./data/Replica/office2 \
-    ./outputs/Replica/office2 \
-    no_viewer
-
-./bin/replica_rgbd \
-    ./ORB-SLAM3/Vocabulary/ORBvoc.txt \
-    ./cfg/ORB_SLAM3/RGB-D/Replica/office3.yaml \
-    ./cfg/gaussian_mapper/RGB-D/Replica/replica_rgbd.yaml \
-    ./data/Replica/office3 \
-    ./outputs/Replica/office3 \
-    no_viewer
-
-./bin/replica_rgbd \
-    ./ORB-SLAM3/Vocabulary/ORBvoc.txt \
-    ./cfg/ORB_SLAM3/RGB-D/Replica/office4.yaml \
-    ./cfg/gaussian_mapper/RGB-D/Replica/replica_rgbd.yaml \
-    ./data/Replica/office4 \
-    ./outputs/Replica/office4 \
-    no_viewer
-
-./bin/replica_rgbd \
-    ./ORB-SLAM3/Vocabulary/ORBvoc.txt \
-    ./cfg/ORB_SLAM3/RGB-D/Replica/room0.yaml \
-    ./cfg/gaussian_mapper/RGB-D/Replica/replica_rgbd.yaml \
-    ./data/Replica/room0 \
-    ./outputs/Replica/room0 \
-    no_viewer
-
-./bin/replica_rgbd \
-    ./ORB-SLAM3/Vocabulary/ORBvoc.txt \
-    ./cfg/ORB_SLAM3/RGB-D/Replica/room1.yaml \
-    ./cfg/gaussian_mapper/RGB-D/Replica/replica_rgbd.yaml \
-    ./data/Replica/room1 \
-    ./outputs/Replica/room1 \
+    ./cfg/ORB_SLAM3/RGB-D/Scannet/common.yaml \
+    ./cfg/gaussian_mapper/RGB-D/Scannet/common.yaml \
+    ./data/Scannet/scene0169_00 \
+    ./outputs/Scannet/scene0169_00 \
     no_viewer
